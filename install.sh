58eba8c192bdb3b5ec2d8b2314c16af51c5380a8
JasonScofield <forever8086@gmail.com>
2022-08-04 11:56:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -152,7 +152,7 @@ install_XrayR() {
         cp dns.json /etc/XrayR/
     fi
     
-    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/missuo/XrayR-V2Board/master/XrayR.sh
+    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/JasonScofield/XrayR-V2Board/master/XrayR.sh
     chmod +x /usr/bin/XrayR
     
     # 设置节点序号
@@ -200,7 +200,7 @@ install_XrayR() {
 
     # Writing json
     echo "正在尝试写入配置文件..."
-    wget https://cdn.jsdelivr.net/gh/missuo/XrayR-V2Board/config.yml -O /etc/XrayR/config.yml
+    wget https://cdn.jsdelivr.net/gh/JasonScofield/XrayR-V2Board/config.yml -O /etc/XrayR/config.yml
     sed -i "s/NodeID:.*/NodeID: ${node_id}/g" /etc/XrayR/config.yml
     sed -i "s/NodeType:.*/NodeType: ${node_type}/g" /etc/XrayR/config.yml
     echo ""
